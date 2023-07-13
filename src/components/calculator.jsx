9ac7f8681d925d17b2fcd0578c215686280fc460
mohammadHemmat <118051265+mohammadHemmat@users.noreply.github.com>
2023-07-13 22:12:58 +0330

    add css, history and connect to the keyboard

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 export default function Calculator() {
     const [calc, setCalc] = useState("");
     const [result, setResult] = useState("");
+    const [history, setHistory] = useState([]);
+    const [keyPressed, setKeyPressed] = useState("");
 
-    const ops = ['/', '*', '-', '+', '.'];
+    const ops = ["/", "*", "-", "+", "."];
 
     const createDigits = () => {
         const digits = [];
 
         for (let i = 1; i < 10; i++) {
-            digits.push(<button className="btn" onClick={() => updateCalc(i.toString())} key={i}>{i}</button>);
+            digits.push(
+                <button
+                    className={`btn ${keyPressed === i.toString() ? "btn" : ""}`}
+                    onClick={() => updateCalc(i.toString())}
+                    key={i}
+                >
+                    {i}
+                </button>
+            );
         }
 
         return digits;
-    }
+    };
 
     const updateCalc = (value) => {
         let newCalc = calc;
@@ -48,6 +59,7 @@ export default function Calculator() {
             setResult("");
         }
     };
+
     const calculate = () => {
         console.log("calculate called");
         if (calc === "") return;
@@ -56,16 +68,16 @@ export default function Calculator() {
             const result = eval(calc);
             if (Number.isFinite(result)) {
                 setCalc(eval(calc).toString());
-                setResult(result.toFixed(2).toString())
+                setResult(result.toFixed(2).toString());
+                setHistory([...history, { calculation: calc, result: result.toFixed(2) }]);
             } else {
-                alert("kos nagoo momen")
+                alert("kos nagoo momen");
                 setResult("");
             }
         } catch (err) {
             setResult("");
         }
-
-    }
+    };
 
     const deleteLast = () => {
         if (calc === "") return;
@@ -84,35 +96,98 @@ export default function Calculator() {
             setResult("");
         }
     };
+
     const clearAll = () => {
         setCalc("");
         setResult("");
     };
+
+    const handleHistoryItemClick = (item) => {
+        setCalc(item.calculation);
+        setResult(item.result);
+    };
+
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            const key = event.key;
+            setKeyPressed(key);
+            if (key === "Enter") {
+                calculate();
+            } else if (key === "Backspace" || key === "Delete") {
+                deleteLast();
+            } else if (key === "Escape") {
+                clearAll();
+            } else if (/^[0-9]$/.test(key)) {
+                updateCalc(key);
+            } else if (ops.includes(key)) {
+                updateCalc(key);
+            }
+        };
+
+        const handleKeyUp = () => {
+            setKeyPressed("");
+        };
+
+        document.addEventListener("keydown", handleKeyPress);
+        document.addEventListener("keyup", handleKeyUp);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyPress);
+            document.removeEventListener("keyup", handleKeyUp);
+        };
+    }, [calc, deleteLast, clearAll]);
+
     return (
         <div className="App">
             <div className="calculator">
+                <div className="history-container ">
+                    <h2 className="text-history">History</h2>
+                    <ul className="history">
+                        {history.map((item, index) => (
+                            <li key={index} onClick={() => handleHistoryItemClick(item)}>
+                                {item.calculation} = {item.result}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
                 <div className="display">
-                    <button className="btn-history"></button>
-                    <span>{result ? '(' + result + ')' : ''}</span> {calc || 0}
+                    <span>{result ? "(" + result + ")" : ""}</span>{calc || 0}
                 </div>
 
                 <div className="operators">
-                    <button className="ac btn-below" onClick={clearAll}>AC</button>
-                    <button className="btn-below" onClick={() => updateCalc('/')}>/</button>
-                    <button className="btn-below" onClick={() => updateCalc('*')}>x</button>
-                    <button className="btn-below" onClick={() => updateCalc('-')}>-</button>
-                    <button className="btn-below" onClick={() => updateCalc('+')}>+</button>
-                    <button className="delete btn-below" onClick={deleteLast}>DEL</button>
+                    <button className="ac btn-below" onClick={clearAll}>
+                        AC
+                    </button>
+                    <button className={`btn-below ${keyPressed === "*" ? "btn-below" : ""}`} onClick={() => updateCalc("/")}>
+                        /
+                    </button>
+                    <button className={`btn-below ${keyPressed === "*" ? "btn-below" : ""}`} onClick={() => updateCalc("*")}>
+                        x
+                    </button>
+                    <button className={`btn-below ${keyPressed === "-" ? "btn-below" : ""}`} onClick={() => updateCalc("-")}>
+                        -
+                    </button>
+                    <button className={`btn-below ${keyPressed === "+" ? "btn-below" : ""}`} onClick={() => updateCalc("+")}>
+                        +
+                    </button>
+                    <button className="delete btn-below btn-rangi" onClick={deleteLast}>
+                        DEL
+                    </button>
                 </div>
 
                 <div className="digits">
                     {createDigits()}
-                    <button className="btn-below" onClick={() => updateCalc('0')}>0</button>
-                    <button className="btn-below" onClick={() => updateCalc('.')}>.</button>
-                    <button className="btn-below" onClick={calculate}>=</button>
+                    <button className={`btn-below ${keyPressed === "0" ? "btn-below" : ""}`} onClick={() => updateCalc("0")}>
+                        0
+                    </button>
+                    <button className={`btn-below ${keyPressed === "." ? "btn-below" : ""}`} onClick={() => updateCalc(".")}>
+                        .
+                    </button>
+                    <button className={`btn-below ${keyPressed === "Enter" ? "btn-below" : ""}`} onClick={calculate}>
+                        =
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
